28296817b4561f5128227cd572a150b3ee147690
zergman.mokujin@gmail.com <zergman.mokujin@gmail.com@74970565-31e3-8e80-cf2a-f212af60e714>
2011-11-08 20:20:05 +0000

    Added StyleCop Added icon Change to .NET 4.0

---
+++ trunk/work-with-subtitle/RecalleLeSousTitre/Utils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace RecalleLeSousTitre
+{
+    public static class Utils
+    {
+        public static bool DiffDebutFin(LigneSousTitre ligne, int seconds)
+        {
+            TimeSpan ts = new TimeSpan(0, 0, seconds);
+            TimeSpan ts2 = ligne.TempsFin.Subtract(ligne.TempsDebut);
+            if (ts.CompareTo(ts2) > 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static Color DiffDebutFinColor(LigneSousTitre ligne, int seconds)
+        {
+            if (DiffDebutFin(ligne,seconds))
+            {
+                return Color.Tomato;
+            }
+            else
+            {
+                return Color.Green;
+            }
+        }
+    }
+}
