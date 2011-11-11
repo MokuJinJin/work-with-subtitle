2aa5bea5e0f4e03b62659ca7814d5054bac3720a
zergman.mokujin@gmail.com <zergman.mokujin@gmail.com@74970565-31e3-8e80-cf2a-f212af60e714>
2011-11-11 15:09:13 +0000

    StyleCop v4.6

--- trunk/work-with-subtitle/RecalleLeSousTitre/Form1.cs
+++ trunk/work-with-subtitle/RecalleLeSousTitre/Form1.cs
@@ -1,21 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using System.IO;
+﻿//-----------------------------------------------------------------------
+// <copyright file="Form1.cs" company="Zed Byt Corp">
+//     Copyright Zed Byt Corp 2010
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace RecalleLeSousTitre
 {
+    using System;
+    using System.IO;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Main form
+    /// </summary>
     public partial class Form1 : Form
     {
-        public string file = string.Empty;
-        List<LigneSousTitre> SSTitre = new List<LigneSousTitre>();
+        /// <summary>
+        /// Loaded file
+        /// </summary>
+        private string file = string.Empty;
+
+        /// <summary>
+        /// UI Controler
+        /// </summary>
         private Controler _controler = new Controler();
 
+        /// <summary>
+        /// Main form Constructor
+        /// </summary>
         public Form1()
         {
             InitializeComponent();
@@ -23,7 +35,12 @@ namespace RecalleLeSousTitre
             file = textBox1.Text;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Browse file to load
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">eventArgs</param>
+        private void Browse_Click(object sender, EventArgs e)
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
@@ -32,6 +49,11 @@ namespace RecalleLeSousTitre
             }
         }
 
+        /// <summary>
+        /// Load file and read it
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">EventArgs</param>
         private void Charger_Click(object sender, EventArgs e)
         {
             _controler.ReadFile(file);
@@ -40,49 +62,73 @@ namespace RecalleLeSousTitre
             AfficheLignes(lignes);
         }
 
-        private void btnNext_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Move to next Subtitle
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">EventArgs</param>
+        private void BtnNext_Click(object sender, EventArgs e)
         {
             Tuple<LigneSousTitre, LigneSousTitre, LigneSousTitre> lignes = _controler.GetNext();
             AfficheLignes(lignes);
         }
 
-        private void btnRewind_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Rewind to before subtitle
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">EventArgs</param>
+        private void BtnRewind_Click(object sender, EventArgs e)
         {
             Tuple<LigneSousTitre, LigneSousTitre, LigneSousTitre> lignes = _controler.GetRewind();
             AfficheLignes(lignes);
         }
 
-        private void btnFirstError_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Set the current subtitle to the first error
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">EventArgs</param>
+        private void BtnFirstError_Click(object sender, EventArgs e)
         {
             Tuple<LigneSousTitre, LigneSousTitre, LigneSousTitre> lignes = _controler.GetFirstError();
             AfficheLignes(lignes);
         }
 
-        private void AfficheLignes(Tuple<LigneSousTitre, LigneSousTitre, LigneSousTitre> lignes)
+        /// <summary>
+        /// Method to show the subtitles
+        /// </summary>
+        /// <param name="lines">Before, after and the current subtitle</param>
+        private void AfficheLignes(Tuple<LigneSousTitre, LigneSousTitre, LigneSousTitre> lines)
         {
-            lblNumeroLigneBefore.Text = lignes.Item1.numero.ToString();
-            lblTempsDebutBefore.Text = lignes.Item1.TempsDebut.ToString();
-            lblTempsFinBefore.Text = lignes.Item1.TempsFin.ToString();
+            lblNumeroLigneBefore.Text = lines.Item1.numero.ToString();
+            lblTempsDebutBefore.Text = lines.Item1.TempsDebut.ToString();
+            lblTempsFinBefore.Text = lines.Item1.TempsFin.ToString();
             lblTexteBefore.Text = string.Empty;
-            lignes.Item1.soustitre.ForEach(s => lblTexteBefore.Text += string.Format("{0}{1}", s, Environment.NewLine));
-            lblColorBefore.BackColor = Utils.DiffDebutFinColor(lignes.Item1, _controler.SecondsError);
+            lines.Item1.soustitre.ForEach(s => lblTexteBefore.Text += string.Format("{0}{1}", s, Environment.NewLine));
+            lblColorBefore.BackColor = Utils.DiffDebutFinColor(lines.Item1, _controler.SecondsError);
 
-            lblNumeroLigne.Text = lignes.Item2.numero.ToString();
-            lblTempsDebut.Text = lignes.Item2.TempsDebut.ToString();
-            lblTempsFin.Text = lignes.Item2.TempsFin.ToString();
+            lblNumeroLigne.Text = lines.Item2.numero.ToString();
+            lblTempsDebut.Text = lines.Item2.TempsDebut.ToString();
+            lblTempsFin.Text = lines.Item2.TempsFin.ToString();
             lblTexte.Text = string.Empty; 
-            lignes.Item2.soustitre.ForEach(s => lblTexte.Text += string.Format("{0}{1}", s, Environment.NewLine));
-            lblColor.BackColor = Utils.DiffDebutFinColor(lignes.Item2, _controler.SecondsError);
+            lines.Item2.soustitre.ForEach(s => lblTexte.Text += string.Format("{0}{1}", s, Environment.NewLine));
+            lblColor.BackColor = Utils.DiffDebutFinColor(lines.Item2, _controler.SecondsError);
 
-            lblNumeroLigneAfter.Text = lignes.Item3.numero.ToString();
-            lblTempsDebutAfter.Text = lignes.Item3.TempsDebut.ToString();
-            lblTempsFinAfter.Text = lignes.Item3.TempsFin.ToString();
+            lblNumeroLigneAfter.Text = lines.Item3.numero.ToString();
+            lblTempsDebutAfter.Text = lines.Item3.TempsDebut.ToString();
+            lblTempsFinAfter.Text = lines.Item3.TempsFin.ToString();
             lblTexteAfter.Text = string.Empty;
-            lignes.Item3.soustitre.ForEach(s => lblTexteAfter.Text += string.Format("{0}{1}", s, Environment.NewLine));
-            lblColorAfter.BackColor = Utils.DiffDebutFinColor(lignes.Item3, _controler.SecondsError);
+            lines.Item3.soustitre.ForEach(s => lblTexteAfter.Text += string.Format("{0}{1}", s, Environment.NewLine));
+            lblColorAfter.BackColor = Utils.DiffDebutFinColor(lines.Item3, _controler.SecondsError);
         }
 
-        private void save_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Write all subtitle lines into a file
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">EventArgs</param>
+        private void Save_Click(object sender, EventArgs e)
         {
             if (saveFileDialog1.ShowDialog() == DialogResult.OK)
             {
@@ -95,34 +141,58 @@ namespace RecalleLeSousTitre
             }
         }
 
-        private void btnFoundErrors_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Show the number of errors into the subtitle
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">EventArgs</param>
+        private void BtnFoundErrors_Click(object sender, EventArgs e)
         {
             MessageBox.Show(_controler.FoundErrors().ToString());
         }
 
-        private void btnRewindError_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Rewind to before error
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">EventArgs</param>
+        private void BtnRewindError_Click(object sender, EventArgs e)
         {
             Tuple<LigneSousTitre, LigneSousTitre, LigneSousTitre> lignes = _controler.GetRewindError();
             AfficheLignes(lignes);
         }
 
-        private void btnNextError_Click(object sender, EventArgs e)
+        /// <summary>
+        /// go to the next error
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">EventArgs</param>
+        private void BtnNextError_Click(object sender, EventArgs e)
         {
             Tuple<LigneSousTitre, LigneSousTitre, LigneSousTitre> lignes = _controler.GetNextError();
             AfficheLignes(lignes);
         }
 
-        private void button6_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Go to the first subtitle
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">EventArgs</param>
+        private void First_Click(object sender, EventArgs e)
         {
             Tuple<LigneSousTitre, LigneSousTitre, LigneSousTitre> lignes = _controler.GetFirst();
             AfficheLignes(lignes);
         }
 
+        /// <summary>
+        /// Change the seocnd between StartTime and EndTime
+        /// to be considerated as an error
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">EventArgs</param>
         private void UpDownSecondsError_ValueChanged(object sender, EventArgs e)
         {
             _controler.SecondsError = int.Parse(UpDownSecondsError.Value.ToString());
         }
-
-        
     }
 }
